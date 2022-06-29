4c9b1d0628370c30375d4266ace9768ab5d44d75
Erhan <erhantezer68@gmail.com>
2022-06-29 23:27:51 +0300

    new project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Card from "./components/card/Card";
+import Header from "./components/header/Header";
+import "./App.css"
+
+
+function App() {
+  return (
+    <div className="main-container" >
+     <h1>Language Card Project</h1>
+     <Header/>
+     <Card />
+    </div>
+  );
+}
+
+export default App;
